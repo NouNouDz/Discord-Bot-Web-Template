5a839ce1cc950f3635dbc122414ec0ffbfad16d9
James Grasso <grassojames5@gmail.com>
2021-01-30 22:24:40 +1100

    Fixed the buttons

--- public/javascripts/html/invite.js
+++ public/javascripts/html/invite.js
@@ -3,6 +3,10 @@ var permissions_id = "PERMISSIONS_ID"; // More info on permissions here:
 
 var url = `https://discord.com/api/oauth2/authorize?client_id=${client_id}&permissions=${permissions_id}&scope=bot`;
 
-document.getElementById('add-to-server-button').onclick = function () {
-    window.open(url, '_blank');
+var invite_buttons = document.getElementsByClassName('invite-btn');
+
+for (i = 0; i < invite_buttons.length; i++) {
+    invite_buttons.item(i).onclick = function () {
+        window.open(url, '_blank');
+    };
 };
