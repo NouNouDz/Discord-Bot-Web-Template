d52245896c3188ceac99870a2a8bd18967163a2e
James Grasso <grassojames5@gmail.com>
2021-01-30 16:47:12 +1100

    Fixed the javascript

--- replacement.js
+++ replacement.js
@@ -1,3 +1,10 @@
+// You just worry about this bit :)
+
+// Put your bot's name in the string below! (for ' use \')
+var botName = 'Your bot\'s name';
+
+
+// Aight this is the complicated bit... Don't worry about this!
 const loremIpsum = require('lorem-ipsum').LoremIpsum;
 
 const lorem = new loremIpsum({
@@ -7,10 +14,28 @@ const lorem = new loremIpsum({
     }
 });
 
-document.getele
-
 var cards = document.getElementsByClassName('card-description');
 
-for (var i = 0; i < cards.length; i++) {
+for (i = 0; i < cards.length; i++) {
     cards.item(i).innerHTML = lorem.generateSentences(3);
 }
+
+var replaceBotName = '[Your Bot Name]';
+
+var replacements = document.getElementsByTagName('h3');
+
+function replace(list, replace, replacement) {
+    for (i = 0; i < list.length; i++) {
+        list.item(i).innerHTML = list.item(i).innerHTML.replace(replace, replacement)
+    }
+}
+
+replace(replacements, replaceBotName, botName);
+
+replacements = document.getElementsByTagName('p');
+
+replace(replacements, replaceBotName, botName);
+
+replacements = document.getElementsByTagName('h1');
+
+replace(replacements, replaceBotName, botName);
