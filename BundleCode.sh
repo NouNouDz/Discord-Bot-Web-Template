55d36053463d7737cbe1f477cbbb6925f9f46370
James Grasso <grassojames5@gmail.com>
2021-01-30 22:02:55 +1100

    Fixed bundlecode.sh

--- BundleCode.sh
+++ BundleCode.sh
@@ -1,7 +1,5 @@
-browserify ./public/javascripts/html/invite.js -o ./public/javascripts/html/bundled/invite.js
-
 browserify ./public/javascripts/html/replacement.js -o ./public/javascripts/html/bundled/rpl.js
 echo Bundled replacement.js
-
+browserify ./public/javascripts/html/invite.js -o ./public/javascripts/html/bundled/invite.js
 echo Bundled invite.js
 wait
