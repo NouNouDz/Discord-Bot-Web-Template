d3af53a4bd675d4096a549cdffa02250b400857a
James Grasso <grassojames5@gmail.com>
2021-01-31 21:40:34 +1100

    Added the ability to add a support page in future

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var supportRouter = require('./routes/support');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -23,6 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/support', supportRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
--- routes/support.js
+++ routes/support.js
@@ -3,9 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'Express'
-  });
+    res.render('support', {
+        title: 'Express'
+    });
 });
 
 module.exports = router;
