ac66bb25267acfef5c9c4abcf76e38784e2d3c0e
James Grasso <grassojames5@gmail.com>
2021-02-01 15:47:20 +1100

    Updated the views

--- routes/support.js
+++ routes/support.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('support', {
+    res.render('support/support', {
         title: 'Express'
     });
 });
